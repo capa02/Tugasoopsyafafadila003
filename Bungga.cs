c9c262735efe008a6f4acbb3f07226463ea37e09
capa02 <syafafadila.04@gmail.com>
2025-09-23 22:51:14 +0700

    Add files via upload

---
+++ Bungga.cs
@@ -0,0 +1,15 @@
+﻿public class Bunga : Tanaman
+{
+    public string WarnaBunga { get; set; }
+
+    public Bunga(string spesies, int usiaBulan, string warna) : base(spesies, usiaBulan)
+    {
+        this.WarnaBunga = warna;
+    }
+
+    public override void Deskripsi()
+    {
+        base.Deskripsi();
+        Console.WriteLine($"Bunga ini memiliki warna {WarnaBunga}.");
+    }
+}
